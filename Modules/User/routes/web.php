10d70798b6be54c1790ff83dbb860a15043f8c37
Laurynnnn <laurynkantono@gmail.com>
2024-10-16 11:03:53 +0300

    few changes

--- Modules/User/routes/web.php
+++ Modules/User/routes/web.php
@@ -63,7 +63,7 @@ Route::get('/email/verify', function () {
 Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
     $request->fulfill();
  
-    return redirect('/home');
+    return redirect('/dashboard');
 })->middleware(['auth', 'signed'])->name('verification.verify');
 
 use Illuminate\Http\Request;
